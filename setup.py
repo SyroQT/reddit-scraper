ea63c55d86c9a071e1c9a43b62bce456acf9815b
Titas <titas.januson@gmail.com>
2021-01-27 14:57:11 +0200

    Add requirements

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
+    install_requires=["bs4", "cleantext", "requests", "pandas"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
